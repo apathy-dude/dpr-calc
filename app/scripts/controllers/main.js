ef44aa4b778a32bcf1ee11e2a812f3edab5abf3c
Thomas Brown <flaron1990@hotmail.com>
2015-05-04 01:23:22 -0400

    Adding character specific information. And allow for editing of fields

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,32 +9,177 @@
  */
 angular.module('dprCalcApp')
   .controller('MainCtrl', function ($scope) {
-      $scope.charactersCount = 0;
+      var charactersCount = 0;
       function emptyCharacter() {
-          var id = $scope.charactersCount;
-          $scope.charactersCount++;
+          var id = charactersCount;
+          charactersCount++;
           return {
-              'name': 'character-' + id
+              'name': 'Character-' + id,
+              'levels': [],
+              'race': 'Human',
+              'class': '',
+              'abilityScores': {
+                  'strength': 10,
+                  'dexterity': 10,
+                  'constitution': 10,
+                  'intelligence': 10,
+                  'wisdom': 10,
+                  'charisma': 10
+              }
           };
       }
 
-      $scope.characters = [emptyCharacter()];
-      $scope.selectedCharacter = 0;
+      function emptyLevel() {
+          var lastLevel = 0;
+          if($scope.selectedCharacter.levels.length > 0) {
+              lastLevel = _.max($scope.selectedCharacter.levels, function(lvl) {
+                  var level = String.parseInt(lvl.level);
+                  return level === Number.NAN ? 0 : level;
+              });
+          }
 
-      $scope.emptyCharacter = emptyCharacter;
+          return {
+              'level': lastLevel + 1,
+              'applyPreviousLevels': true,
+              'abilityScoreChanges': {
+                  'strength': 0,
+                  'dexterity': 0,
+                  'constitution': 0,
+                  'inteligence': 0,
+                  'wisdom': 0,
+                  'charisma': 0
+              },
+              'abilityScoreEnhancements': {
+                  'strength': 0,
+                  'dexterity': 0,
+                  'constitution': 0,
+                  'inteligence': 0,
+                  'wisdom': 0,
+                  'charisma': 0
+              },
+              'attackGroups': [],
+              'equipment': [],
+              'feats': [],
+              'skills': [],
+              'ac': { 'base': { 'value': 10, 'flat-footed': true, 'touch': true } },
+              'saves': {
+                  'fortitude': { 'base': 0, 'abilityScores': ['constitution'] },
+                  'reflex': { 'base': 0, 'abilityScores': ['dexterity'] },
+                  'will': { 'base': 0, 'abilityScores': ['wisdom'] }
+              }
+
+          };
+      }
+
+      // Character tab management
+      $scope.characters = [];
+      $scope.selectedCharacterIndex = -1;
+      $scope.selectedCharacter = null;
+
       $scope.selectCharacter = function(ind) {
-          $scope.selectedCharacter = ind;
+          $scope.clearEdit();
+          $scope.selectedCharacterIndex = ind;
+
+          if(ind === -1) {
+              $scope.selectedCharacter = null;
+          }
+          else {
+              $scope.selectedCharacter = $scope.characters[ind];
+          }
+      };
+      $scope.addCharacter = function() {
+          $scope.characters.push(emptyCharacter());
+          $scope.selectCharacter($scope.characters.length - 1);
       };
       $scope.removeCharacter = function(ind) {
           $scope.characters.splice(ind, 1);
-          if($scope.selectedCharacter > ind) {
-              $scope.selectedCharacter--;
+          if($scope.selectedCharacterIndex > ind) {
+              $scope.selectCharacter($scope.selectedCharacterIndex - 1);
           }
           else if($scope.characters.length === 0) {
-              $scope.selectedCharacter = 0;
+              $scope.selectCharacter(-1);
           }
-          else if($scope.selectedCharacter > $scope.characters.length - 1) {
-              $scope.selectedCharacter = $scope.characters.length - 1;
+          else if($scope.selectedCharacterIndex > $scope.characters.length - 1) {
+              $scope.selectCharacter($scope.characters.length - 1);
           }
       };
-  });
+
+      // Level tab management
+      $scope.selectedLevelIndex = -1;
+      $scope.selectedLevel = null;
+      $scope.selectLevel = function(ind) {
+          $scope.clearEdit();
+          $scope.selectedLevelIndex = ind;
+
+          if(ind === -1) {
+              $scope.selectedLevel = null;
+          }
+          else {
+              $scope.selectedLevel = $scope.selectedCharacter.level[ind];
+          }
+      };
+      $scope.addLevel = function() {
+          $scope.selectedCharacter.push(emptyLevel());
+          $scope.selectedLevel = $scope.selectLevel($scope.selectedCharacter.levels.length - 1);
+      };
+      $scope.removeLevel = function(ind) {
+          $scope.selectedCharacter.levels.splice(ind, 1);
+          if($scope.selectedLevelIndex > ind) {
+              $scope.selectLevel($scope.selectedLevelIndex - 1);
+          }
+          else if($scope.selectedCharacter.levels.length === 0) {
+              $scope.selectLevel(-1);
+          }
+          else if($scope.selectedLevelIndex > $scope.selectedCharacter.levels.length -1){
+              $scope.selectLevel($scope.characters.length -1);
+          }
+      };
+
+      // Edit management
+      $scope.edit = null;
+      $scope.editTemp = null;
+      $scope.setEdit = function(field, temp) {
+          $scope.edit = field;
+          if(temp) {
+              var target = $scope;
+              var fields = temp.split('.');
+              while(fields.length > 1) {
+                  target = target[fields.shift()];
+              }
+              $scope.editTemp = { field: temp, value: target[fields[0]] };
+          }
+          else {
+              $scope.editTemp = null;
+          }
+      };
+      $scope.clearEdit = function() {
+          $scope.edit = null;
+      };
+      $scope.cancelEdit = function() {
+          if($scope.editTemp === null) {
+              return;
+          }
+
+          var target = $scope;
+          var fields = $scope.editTemp.field.split('.');
+          while(fields.length > 1) {
+              target = target[fields.shift()];
+          }
+
+          target[fields[0]] = $scope.editTemp.value;
+          $scope.editTemp = null;
+
+          $scope.clearEdit();
+      };
+
+      // Character management
+      $scope.getAbilitymod = function(score) {
+          score = Number.parseInt(score);
+          return Math.floor((score - 10) / 2);
+      };
+      $scope.abilityScores = ['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'];
+
+      // Initialization
+      $scope.addCharacter();
+  }
+);
