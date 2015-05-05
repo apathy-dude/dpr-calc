5705c474c48f1fc29679d0b8a46d962e253be1e2
Thomas Brown <flaron1990@hotmail.com>
2015-05-05 00:59:07 -0400

    Added ability scores. Moved to not assuming the use of selectedCharacter or level. Initial dpr calc functions ready

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,33 @@
 angular.module('dprCalcApp')
   .controller('MainCtrl', function ($scope) {
       var charactersCount = 0;
+      var BONUS_TYPE = {
+          static: 0,
+          ability: 1,
+          dynamic: 2,
+          bab: 3
+      };
+
+      function getValue(character, level, bonus) {
+          var value;
+          switch(bonus.type) {
+              case BONUS_TYPE.static: value = bonus.value; break;
+              case BONUS_TYPE.dynamic: value = bonus.value; break;
+              case BONUS_TYPE.ability:
+                  var temp = $scope.getAbilityScore(character, level, bonus.value);
+                  value = $scope.getAbilityMod(temp);
+                  break;
+              case BONUS_TYPE.bab: value = 0; break;
+          }
+
+          if(bonus.modifier) {
+              value *= bonus.modifier;
+              value = Math.floor(value);
+          }
+
+          return value;
+      }
+
       function emptyCharacter() {
           var id = charactersCount;
           charactersCount++;
@@ -36,18 +63,17 @@ angular.module('dprCalcApp')
           if($scope.selectedCharacter.levels.length > 0) {
               lastLevel = _.max($scope.selectedCharacter.levels, function(lvl) {
                   var level = Number.parseInt(lvl.level);
-                  return level === Number.NAN ? 0 : level;
+                  return isNaN(level) ? 0 : level;
               }).level;
           }
 
           return {
               'level': lastLevel + 1,
-              'applyPreviousLevels': true,
               'abilityScoreChanges': {
                   'strength': 0,
                   'dexterity': 0,
                   'constitution': 0,
-                  'inteligence': 0,
+                  'intelligence': 0,
                   'wisdom': 0,
                   'charisma': 0
               },
@@ -55,7 +81,7 @@ angular.module('dprCalcApp')
                   'strength': 0,
                   'dexterity': 0,
                   'constitution': 0,
-                  'inteligence': 0,
+                  'intelligence': 0,
                   'wisdom': 0,
                   'charisma': 0
               },
@@ -63,13 +89,106 @@ angular.module('dprCalcApp')
               'equipment': [],
               'feats': [],
               'skills': [],
-              'ac': { 'base': { 'value': 10, 'flat-footed': true, 'touch': true } },
+              'ac': {
+                  'base': { 'type': BONUS_TYPE.static, 'value': 10, 'flat-footed': true, 'touch': true },
+                  'dexterity': { 'type': BONUS_TYPE.ability, 'value': 'dexterity', 'flat-footed': false, 'touch': true },
+                  'armor': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': false },
+                  'shield': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': false },
+                  'natural': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': false },
+                  'dodge': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': false, 'touch': true },
+                  'deflection': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': true },
+                  'size': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': true },
+                  'touch': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': false, 'touch': true },
+                  'flat-footed': { 'type': BONUS_TYPE.dynamic, 'value': 0, 'flat-footed': true, 'touch': false }
+              },
               'saves': {
-                  'fortitude': { 'base': 0, 'abilityScores': ['constitution'] },
-                  'reflex': { 'base': 0, 'abilityScores': ['dexterity'] },
-                  'will': { 'base': 0, 'abilityScores': ['wisdom'] }
-              }
+                  'fortitude': {
+                      'base': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'constitution': { 'type': BONUS_TYPE.ability, 'value': 'constitution' },
+                      'magic': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'misc': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  },
+                  'reflex': {
+                      'base': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'dexterity': { 'type': BONUS_TYPE.ability, 'value': 'dexterity' },
+                      'magic': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'misc': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  },
+                  'will': {
+                      'base': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'wisdom': { 'type': BONUS_TYPE.ability, 'value': 'wisdom' },
+                      'magic': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                      'misc': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  }
+              },
+              'dr': 0,
+              'sr': 0,
+              'hp-gain': {
+                  'level': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  'constitution': { 'type': BONUS_TYPE.ability, 'value': 'constitution' },
+                  'favoured': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  'toughness': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+                  'other': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+              },
+              'bab-gain': 0,
+              'movement': {
+                  'base': 30,
+                  'armor': 30,
+                  'fly': 0,
+                  'swim': 0,
+                  'climb': 0,
+                  'burrow': 0
+              },
+              'initiative': {
+                  'dexterity': { 'type': BONUS_TYPE.ability, 'value': 'dexterity' },
+                  'misc': { 'type': BONUS_TYPE.dynamic, 'value': 0 },
+              },
+              'spell-casting': [],
+              'abilities': [],
+          };
+      }
 
+      function emptyAttackGroup() {
+          return {
+              'name': '',
+              'attacks': []
+          };
+      }
+
+      function emptyAttack() {
+          return {
+            'weapon': '',
+            'damageDice': [],
+            'damageBonus': {},
+            'hitChance': {},
+            'critThreat': 0.05,
+            'critMultiplier': 2
+          };
+      }
+
+      function emptyMeleeAttack() {
+          return {
+              'weapon': '',
+              'damage-dice': [],
+              'damage-bonus': {
+                  'strength': { 'type': BONUS_TYPE.ability, 'value': 'strength' },
+              },
+              'hit-chance': {
+                  'strength': { 'type': BONUS_TYPE.ability, 'value': 'strength' },
+                  'bab': { 'type': BONUS_TYPE.bab, 'value': 'bab' },
+              }
+          };
+      }
+
+      function emptyRangedAttack() {
+          return {
+              'weapon': '',
+              'damage-dice': [],
+              'damage-bonus': {},
+              'hit-chance': {
+                  'strength': { 'type': BONUS_TYPE.ability, 'value': 'dexterity' },
+                  'bab': { 'type': BONUS_TYPE.bab, 'value': 'bab' },
+              }
           };
       }
 
@@ -92,14 +211,21 @@ angular.module('dprCalcApp')
       $scope.addCharacter = function() {
           $scope.characters.push(emptyCharacter());
           $scope.selectCharacter($scope.characters.length - 1);
+          $scope.addLevel($scope.selectedCharacter);
       };
       $scope.removeCharacter = function(ind) {
-          $scope.characters.splice(ind, 1);
-          if($scope.selectedCharacterIndex > ind) {
-              $scope.selectCharacter($scope.selectedCharacterIndex - 1);
+          if($scope.characters.length === 1) {
+              return;
           }
-          else if($scope.characters.length === 0) {
-              $scope.selectCharacter(-1);
+
+          $scope.characters.splice(ind, 1);
+          if($scope.selectedCharacterIndex >= ind) {
+              if($scope.selectedCharacterIndex === 0 || ind === 0) {
+                  $scope.selectCharacter(0);
+              }
+              else {
+                  $scope.selectCharacter($scope.selectedCharacterIndex - 1);
+              }
           }
           else if($scope.selectedCharacterIndex > $scope.characters.length - 1) {
               $scope.selectCharacter($scope.characters.length - 1);
@@ -107,8 +233,7 @@ angular.module('dprCalcApp')
       };
 
       // Level tab management
-      $scope.selectLevel = function(ind) {
-          var character = $scope.selectedCharacter;
+      $scope.selectLevel = function(character, ind) {
           $scope.clearEdit();
           character.selectedLevelIndex = ind;
 
@@ -119,23 +244,33 @@ angular.module('dprCalcApp')
               character.selectedLevel = $scope.selectedCharacter.levels[ind];
           }
       };
-      $scope.addLevel = function() {
-          var character = $scope.selectedCharacter;
-          $scope.selectedCharacter.levels.push(emptyLevel());
-          character.selectedLevel = $scope.selectLevel($scope.selectedCharacter.levels.length - 1);
+      $scope.addLevel = function(character) {
+          character.levels.push(emptyLevel());
+          $scope.selectLevel(character, character.levels.length - 1);
       };
-      $scope.removeLevel = function(ind) {
-          var character = $scope.selectedCharacter;
-          $scope.selectedCharacter.levels.splice(ind, 1);
-          if(character.selectedLevelIndex > ind) {
-              $scope.selectLevel(character.selectedLevelIndex - 1);
+      $scope.removeLevel = function(character, ind) {
+          if(character.levels.length === 1) {
+              return;
           }
-          else if($scope.selectedCharacter.levels.length === 0) {
-              $scope.selectLevel(-1);
+
+          character.levels.splice(ind, 1);
+          if(character.selectedLevelIndex >= ind) {
+              if(ind === 0 || character.selectedLevelIndex === 0) {
+                  $scope.selectLevel(character, 0);
+              }
+              else {
+                  $scope.selectLevel(character, character.selectedLevelIndex - 1);
+              }
           }
-          else if(character.selectedLevelIndex > $scope.selectedCharacter.levels.length -1){
-              $scope.selectLevel($scope.characters.length -1);
+          else if(character.selectedLevelIndex > character.levels.length -1){
+              $scope.selectLevel(character, $scope.characters.length -1);
           }
+      };
+      $scope.sortLevel = function(character) {
+          character.levels = _.sortBy(character.levels, function(lvl) {
+              return Number.parseInt(lvl.level);
+          });
+          character.selectedLevelIndex = _.indexOf(character.levels, character.selectedLevel);
       };
 
       // Edit management
@@ -176,13 +311,117 @@ angular.module('dprCalcApp')
       };
 
       // Character management
-      $scope.getAbilitymod = function(score) {
+      $scope.getAbilityScore = function(character, level, score) {
+          var lev = Number.parseInt(level.level);
+          var initialValue = Number.parseInt(character.abilityScores[score]);
+          var enhancement = Number.parseInt(level.abilityScoreEnhancements[score]);
+
+          var val = _.chain(character.levels)
+            .filter(function(lvl) {
+                var l = Number.parseInt(lvl.level);
+                return l <= lev;
+            })
+            .reduce(function(result, value) {
+                var v = Number.parseInt(value.abilityScoreChanges[score]);
+                return result += v ? v : 0;
+            }, initialValue ? initialValue : 0)
+            .value();
+          
+          val += enhancement ? enhancement : 0;
+
+          return val;
+      };
+      $scope.getAbilityMod = function(score) {
           score = Number.parseInt(score);
           return Math.floor((score - 10) / 2);
       };
+      $scope.getPointBuy = function(character) {
+          if(!character) {
+              return;
+          }
+
+          return _.reduce($scope.abilityScores, function(result, score) {
+              var val = Number.parseInt(character.abilityScores[score]);
+              val = val < 3 ? 3 : val;
+              val = val > 18 ? 18 : val;
+              return result += $scope.pointBuy[val ? val : 10];
+          }, 0);
+      };
+
+      // Attack management
+      function calculateAttackDPR(character, level, attack, targetAC) {
+          var hitChance;
+          var minHitChance = 0.05;
+          var maxHitChance = 0.95;
+          var damage = 0;
+          var percision = 0;
+          var hit = _.reduce(attack.hitChance, function(total, chance) {
+              return total += getValue(character, level, chance);
+          }, 0);
+
+          for(var d in attack.damageDice) {
+              var die = attack.damageDice[d];
+              if(die.percision) {
+                  percision += die.average();
+              }
+              else {
+                  damage += die.average();
+              }
+          }
+
+          for(var b in attack.damageBonus) {
+              var bonus = attack.damageBonus[b];
+              if(bonus.percision) {
+                  percision += getValue(character, level, bonus);
+              }
+              else {
+                  damage += getValue(character, level, bonus);
+              }
+          }
+
+          hitChance = 1 - (targetAC-hit) / 20;
+          hitChance = hitChance < minHitChance ? minHitChance : hitChance;
+          hitChance = hitChance < maxHitChance ? maxHitChance : hitChance;
+
+          // h(dp)+c(m-1)hd
+          return hitChance * (damage + percision) + attack.critChance * (attack.critMultiplier - 1) * hitChance * damage;
+      }
+      $scope.calculateDPR = function(character, level, attackGroup, targetAC) {
+          return _.reduce(attackGroup.attacks, function(total, attack) {
+              return total += calculateAttackDPR(character, level, attack, targetAC);
+          }, 0);
+      };
+
+      // Static data
       $scope.abilityScores = ['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'];
+      $scope.pointBuy = {
+          3: -4,
+          4: -4,
+          5: -4,
+          6: -4,
+          7: -4,
+          8: -2,
+          9: -1,
+          10: 0,
+          11: 1,
+          12: 2,
+          13: 3,
+          14: 5,
+          15: 7,
+          16: 10,
+          17: 13,
+          18: 17
+      };
 
       // Initialization
       $scope.addCharacter();
+
+      //Not used functions
+      (function() {
+          emptyAttackGroup();
+          emptyAttack();
+          emptyMeleeAttack();
+          emptyRangedAttack();
+      })();
   }
 );
